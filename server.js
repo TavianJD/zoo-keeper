f1eb9ad947cc8e55cf9b0764157b6e8ed1f4deaa
hatetavian <tavianjdawson123@gmail.com>
2021-12-14 21:16:44 -0600

    completed GET and POST requests

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const { animals } = require('./data/animals.json');
+const fs = require('fs');
+const path = require('path');
+const e = require('express');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+//parse incoming json data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -49,6 +57,18 @@ function findById(id, animalsArray) {
     return result;
 };
 
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    // our function's main code will go here!
+    animalsArray.push(animal)
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray}, null, 2)
+    );
+    // return finished code to post route for response
+    return animal;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals
     if (req.query) {
@@ -65,6 +85,39 @@ app.get('/api/animals/:id', (req, res) => {
         res.send(404);
     }
 });
+
+app.post('/api/animals', (req, res) => {
+    // req.body is where our incoming content will be
+    //set is based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // if any data is false in req.body, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send("The animal is not properly formatted.")
+    } else {
+        // add animal to json file and animals array in this function
+        const animal = createNewAnimal(req.body, animals)
+        res.json(animal)
+
+    }
+
+});
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+  }
 
 app.listen(PORT, () => {
     console.log(`API sevrer now on port ${PORT}`)
